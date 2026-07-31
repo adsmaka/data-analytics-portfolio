b67d6304d4d09a35065f5598b444a688d3f41c01
adsmaka <adsmaka@outlook.com>
2026-07-31 15:27:44 +1000

    Create 04_create_master_view.sql

---
+++ project-01-nsw-emergency-department/sql/04_create_master_view.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE VIEW `nsw-health-analytics.nsw_hospital_data.vw_ed_performance_cleaned` AS
+SELECT 
+    entity_id AS facility_code,
+    entity_longname AS hospital_name,
+    lhd_shortname AS local_health_district,
+    peer_group_shortname AS peer_group,
+    reporting_period,
+    reporting_level,
+    measure_name,
+    metric AS unit_of_measure,
+    SAFE_CAST(measure_value AS FLOAT64) AS measure_value_num
+FROM `nsw-health-analytics.nsw_hospital_data.ed_performance`
+WHERE measure_value IS NOT NULL;
